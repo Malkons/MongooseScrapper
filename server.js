743fa88a2a7ed8442c2b2abf63c743730d0e34b8
Malkons <marc.alkons@gmail.com>
2018-11-16 21:18:45 -0800

    server changed back

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 if (process.env.MONGODB_URI) {
-  mongoose.connect("mongodb://heroku_hvjrlv8b:45mier7ii4hudksne3rmv4ienr@ds163226.mlab.com:63226/heroku_hvjrlv8b");
+  mongoose.connect(process.env.MONGODB_URI);
 } else {
   mongoose.connect(
     "mongodb://localhost/unit18Populator",
